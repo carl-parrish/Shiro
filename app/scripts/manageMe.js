4db38d7d4bee4cb1d19631ded90984a28b6dc32f
cparrish <cparrish@carlparrish.com>
2015-07-27 12:54:30 -0700

    setup for routing

--- app/scripts/manageMe.js
+++ app/scripts/manageMe.js
@@ -1,23 +1,36 @@
 {
-    var app = angular.module('manageMeApp', ["firebase"]);
-        app.controller('TagsController', function($scope, $firebaseObject, $log){
-       var ref = new Firebase("https://glowing-heat-9560.firebaseio.com");
-       this.catagory = $firebaseObject(ref);
+  var manageMeApp = angular.module('manageMeApp', ['firebase', 'ngRoute']);
+  manageMeApp.config(function ($routeProvider) {
 
   });
-  app.controller("ContactFormController", function(){
-      this.contactEntries = {};
-      this.addContactEntry = function(entry){
-      };
+  manageMeApp.controller('TagsController', function($scope, $firebaseObject, $log){
+    var ref = new Firebase('https://glowing-heat-9560.firebaseio.com');
+    this.catagory = $firebaseObject(ref);
+
+  });
+  manageMeApp.controller('ContactFormController', function(){
+    this.contactEntries = {};
+    this.addContactEntry = function (entry) {
+
+    };
+
   });
 
-  app.directive("listTags", function(){
-      return {
-          restrict: "E",
-          templateUrl: "/scripts/directives/listTags.html",
-          controller: "TagsController",
-          controllerAs: "tags"
-      };
+  manageMeApp.controller('BlogFormController',function(){
+
+    this.calculateWordCount = function(){
+      this.wordCount = blog-input.matches(/\b\w+w\b/gi);
+    }
+
+  });
+
+  manageMeApp.directive('listTags', function(){
+    return {
+      restrict: 'E',
+      templateUrl: '/scripts/directives/listTags.html',
+      controller: "TagsController",
+      controllerAs: 'tags'
+    };
   });
 
 };
